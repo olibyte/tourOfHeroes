d49e04a1369dbd8d0ec23a554935b5da131d314a
Oliver <ocben1@gmail.com>
2020-06-09 08:29:47 +1000

    create a feature component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,14 +9,16 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  
+  selectedHero: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
